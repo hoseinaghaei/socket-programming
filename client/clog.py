69fc5ab8ff72fa976cc8ca2bef59eee2c24fdb25
hossein.aghaei <hossein.aghaei@digikala.com>
2023-03-30 15:09:03 +0330

    add get request log.

--- client/clog.py
+++ client/clog.py
@@ -1,7 +1,7 @@
 from entity import *
 import threading as thread
 
-__get_file_logs = {}
+__get_file_logs = []
 __get_file_lock = thread.Lock()
 __send_file_logs = []
 __send_file_lock = thread.Lock()
@@ -11,7 +11,7 @@ def create_get_file_log(file_name: str, peer: str, seeders: list) -> None:
     global __get_file_logs, __get_file_lock
     __get_file_lock.acquire(blocking=True, timeout=1)
     log = GetFileLog(file_name=file_name, peer=peer, seeders=seeders)
-    __get_file_logs[file_name] = log
+    __get_file_logs.append(log)
     __get_file_lock.release()
 
 
@@ -21,3 +21,19 @@ def create_share_file_log(file_name: str, peer: str, success: bool) -> None:
     log = SendFileLog(file_name=file_name, peer=peer, success=success)
     __send_file_logs.append(log)
     __send_file_lock.release()
+
+
+def get_request_logs():
+    global __get_file_logs, __send_file_logs
+    get_file = []
+    for i in __get_file_logs:
+        get_file.append(i.get_log_formatted())
+
+    send_file = []
+    for i in __send_file_logs:
+        send_file.append(i.get_log_formatted())
+
+    return {
+        'get_log': get_file,
+        'send_log': send_file
+    }
